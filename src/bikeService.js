7fdc904505360ff5d2711f12dfd390925bdf7faa
kymani stephens <kymanist00@gmail.com>
2023-04-20 12:05:52 -0700

    save2

--- src/bikeService.js
+++ src/bikeService.js
@@ -1,13 +1,18 @@
-export default function getBikeInfo() {
+// import { handleSubmit } from "./index.js";
+
+
+
+export default function getBikeInfo(colorSelection, citySelection) {
   let request = new XMLHttpRequest();
-  const url = 'https://bikeindex.org:443/api/v3/search?page=1&per_page=100&location=Portland%2C%20Oregon&stolenness=proximity';
+  const url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=100&colors=${colorSelection}&location=${citySelection}&distance=10&stolenness=proximity`;
 
   request.addEventListener("loadend", function () {
     const response = JSON.parse(this.responseText);
     if (this.status === 200) {
       countColor(getColors(response));
+  
     } else {
-      // printError(this);
+      // printError(this); 
     }
   });
 
@@ -35,58 +40,58 @@ function getColors(apiResponse) {
 
 function countColor(array) {
 
-     let blackCount = 0;
- let blueCount = 0;
- let greenCount = 0;
- let brownCount = 0;
- let redCount = 0;
- let silverCount = 0;
- let tealCount = 0;
- let whiteCount = 0;
- let yellowCount = 0;
- let orangeCount = 0;
- let pinkCount = 0;
- let purpleCount = 0;
+  let blackCount = 0;
+  let blueCount = 0;
+  let greenCount = 0;
+  let brownCount = 0;
+  let redCount = 0;
+  let silverCount = 0;
+  let tealCount = 0;
+  let whiteCount = 0;
+  let yellowCount = 0;
+  let orangeCount = 0;
+  let pinkCount = 0;
+  let purpleCount = 0;
 
- for (let i = 0; i < array.length; i++) {
-   if (array[i] === 'Black') {
-     blackCount++;
-   } else if (array[i] === 'Blue') {
-     blueCount++;
-  } else if (array[i] === 'Green') {
-    greenCount++;
-//   //   } else if (array[i] === 'Brown') {
-//   //     brownCount++;
-//   //   } else if (array[i] === 'Red') {
-//   //     redCount++;
-//   //   } else if (array[i] === 'Silver, gray or bare metal') {
-//   //     silverCount++;
-//   //   } else if (array[i] === 'Teal') {
-//   //     tealCount++;
-//   //   } else if (array[i] === 'Yellow or Gold') {
-//   //     yellowCount++;
-//   //   } else if (array[i] === 'White') {
-//   //     whiteCount++;
-//   //   } else if (array[i] === 'Orange') {
-//   //     orangeCount++;
-//   //   } else if (array[i] === 'Pink') {
-//   //     pinkCount++;
-//   //   } else if (array[i] === 'Purple') {
-//   //     purpleCount++;
-//   //   }
-//   // }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'Black') {
+      blackCount++;
+    } else if (array[i] === 'Blue') {
+      blueCount++;
+    } else if (array[i] === 'Green') {
+      greenCount++;
+    } else if (array[i] === 'Brown') {
+      brownCount++;
+    } else if (array[i] === 'Red') {
+      redCount++;
+    } else if (array[i] === 'Silver, gray or bare metal') {
+      silverCount++;
+    } else if (array[i] === 'Teal') {
+      tealCount++;
+    } else if (array[i] === 'Yellow or Gold') {
+      yellowCount++;
+    } else if (array[i] === 'White') {
+      whiteCount++;
+    } else if (array[i] === 'Orange') {
+      orangeCount++;
+    } else if (array[i] === 'Pink') {
+      pinkCount++;
+    } else if (array[i] === 'Purple') {
+      purpleCount++;
+    }
+  }
 
-//   // console.log(`Black: ${blackCount}`);
-//   // console.log(`Blue: ${blueCount}`);
-//   // console.log(`Green: ${greenCount}`);
-//   // console.log(`Brown: ${brownCount}`);
-//   // console.log(`Red: ${redCount}`);
-//   // console.log(`Silver, gray or bare metal: ${silverCount}`);
-//   // console.log(`Teal: ${tealCount}`);
-//   // console.log(`Yellow or Gold: ${yellowCount}`);
-//   // console.log(`White: ${whiteCount}`);
-//   // console.log(`Orange: ${orangeCount}`);
-//   // console.log(`Pink: ${pinkCount}`);
-//   // console.log(`Purple: ${purpleCount}`);
+  console.log(`Black: ${blackCount}`);
+  console.log(`Blue: ${blueCount}`);
+  console.log(`Green: ${greenCount}`);
+  console.log(`Brown: ${brownCount}`);
+  console.log(`Red: ${redCount}`);
+  console.log(`Silver, gray or bare metal: ${silverCount}`);
+  console.log(`Teal: ${tealCount}`);
+  console.log(`Yellow or Gold: ${yellowCount}`);
+  console.log(`White: ${whiteCount}`);
+  console.log(`Orange: ${orangeCount}`);
+  console.log(`Pink: ${pinkCount}`);
+  console.log(`Purple: ${purpleCount}`);
 
-// }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import getBikeInfo from "./bikeService";
 
+export function handleSubmit(event) {
+  event.preventDefault();
+  const colorSelection = document.getElementById('bike-color').value;
+  const citySelection = document.getElementById('city').value;
+  console.log(colorSelection);
+  console.log(citySelection);
 
+  getBikeInfo(colorSelection, citySelection);
 
+}
 
+const form = document.getElementById('form');
 
-window.addEventListener("load", getBikeInfo);
+form.addEventListener("submit", handleSubmit);
